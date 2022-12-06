6bd76c8e5a49a802999da34b1a7f7ccc1f303637
Chris Adebiyi <42304968+Hackinubee@users.noreply.github.com>
2022-12-06 11:24:07 +0100

    Update settings.py

--- social_ng/settings.py
+++ social_ng/settings.py
@@ -1,6 +1,5 @@
 """
 Django settings for social_ng project.
-
 """
 
 from pathlib import Path
@@ -132,7 +131,7 @@ MAX_UPLOAD_SIZE = "1048576"
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
